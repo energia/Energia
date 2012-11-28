b01937a291379624a0eef313d1e67542cf6f8965
Yining Yang <y.yang@ti.com>
2012-11-28 14:49:42 -0600

    Examples for TMP006

---
+++ hardware/msp430/libraries/Tmp006/examples/TMP_Sleep_Wake/TMP_Sleep_Wake/TMP_Sleep_Wake.ino
@@ -0,0 +1,15 @@
+#include <Tmp006.h>
+
+void setup()
+{ // Initalizes the TMP006 for operation and for I2C communication
+  TMP006_Init(Eight);  // Takes 8 averaged samples for measurement
+  TMP006_Sleep(); // TMP006 enters sleep mode
+  TMP006_Wakeup(); // TMP006 wakes up without need to reconfigure
+}
+
+
+void loop()
+{
+  
+}
+
