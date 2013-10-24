f13523f15ef7a8cfba3cee2f70830b1c8ea4af96
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2013-10-24 00:07:25 -0700

    fix pretty prints

--- hardware/lm4f/cores/lm4f/Client.h
+++ hardware/lm4f/cores/lm4f/Client.h
@@ -1,5 +1,5 @@
-#ifndef		client_h
-#define		client_h
+#ifndef	client_h
+#define	client_h
 #include	"Print.h"
 #include	"Stream.h"
 #include	"IPAddress.h"
@@ -7,14 +7,14 @@
 class Client : public Stream {
 
 public:
-	virtual int		connect(IPAddress ip, uint16_t port) =0;
-	virtual int		connect(const char *host, uint16_t port) =0;
+	virtual int	connect(IPAddress ip, uint16_t port) =0;
+	virtual int	connect(const char *host, uint16_t port) =0;
 	virtual size_t	write(uint8_t) =0;
 	virtual size_t	write(const uint8_t *buf, size_t size) =0;
-	virtual int		available() = 0;
-	virtual int		read() = 0;
-	virtual int		read(uint8_t *buf, size_t size) = 0;
-	virtual int		peek() = 0;
+	virtual int	available() = 0;
+	virtual int	read() = 0;
+	virtual int	read(uint8_t *buf, size_t size) = 0;
+	virtual int	peek() = 0;
 	virtual void	flush() = 0;
 	virtual void	stop() = 0;
 	virtual uint8_t	connected() = 0;
