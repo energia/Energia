9d46359aa46c917c7d11199890930484981d6393
Rei Vilo <rei_vilo@yahoo.fr>
2015-06-05 16:11:30 +0200

    Event library with choice of event number

--- build/shared/examples/MultiTasking/EventLibrary/EventLibrary.ino
+++ build/shared/examples/MultiTasking/EventLibrary/EventLibrary.ino
@@ -1,3 +1,21 @@
+/*
+ * This Sketch demonstrates the Event library that uses TI-RTOS's event feature.
+ * The Event library has 3 functions.
+ * - begin() will setup the button library.
+ * - send() Which will send an event to anybody waiting for an event.
+ * - waitFor() waits for the event to be send and sleeps otherwise.
+ * 
+ * This library will allows you to send an event in one task and have another task
+ * wait for the event. The task waiting for the event to be send
+ * will sleep until the other task sends the event. 
+ *
+ * In this example Sketch, the EventLibrary tab is the task sending the event and does this every
+ * 1000 ms. The Serial tab is the tab waiting for the event to be send.
+ * 
+ * Usage:
+ * Upload the Sketch and open the Serial monitor.
+ */
+ 
 ///
 /// @file		EventLibrary.ino
 /// @brief		Main sketch
